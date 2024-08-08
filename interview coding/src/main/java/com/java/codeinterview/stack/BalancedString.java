2a0856a54afbf53cb5705cf8aed8c76551474de1
rahul mandal <mandalrahul52@gmail.com>
2024-08-08 15:51:29 +0530

    --code interviews

--- interview coding/src/main/java/com/java/codeinterview/stack/BalancedString.java
+++ interview coding/src/main/java/com/java/codeinterview/stack/BalancedString.java
@@ -4,34 +4,36 @@ import java.util.Stack;
 
 public class BalancedString {
 
+    private static final String BALANCED = "Balanced";
+    private static final String NOT_BALANCED = "Not Balanced";
+
     public static void main(String[] args) {
 
         String input = "()[{}()]";
+        if (input.isEmpty()) {
+            System.out.println(NOT_BALANCED);
+        }
         System.out.println(checkBalancedString(input));
     }
 
     private static String checkBalancedString(String input) {
-
-        if (input.isEmpty()) {
-            return "Not Balanced";
-        }
-
         Stack<Character> stack = new Stack<>();
         char[] charArray = input.toCharArray();
         for (Character currentCharacter : charArray) {
             if (currentCharacter == '(' || currentCharacter == '[' || currentCharacter == '{') {
                 stack.push(currentCharacter);
-            }
-            if (stack.empty()) {
-                return "Not Balanced";
+            } else if (stack.empty()) {
+                return NOT_BALANCED;
             } else {
                 Character lastCharacter = stack.peek();
-                if ((lastCharacter == '(' && currentCharacter == ')') ||
-                        (lastCharacter == '{' && currentCharacter == '}') ||
-                        (lastCharacter == '[' && currentCharacter == ']'))
+                if (
+                        (lastCharacter == '(' && currentCharacter == ')') ||
+                                (lastCharacter == '{' && currentCharacter == '}') ||
+                                (lastCharacter == '[' && currentCharacter == ']')
+                )
                     stack.pop();
             }
         }
-        return stack.empty() ? "Balanced" : "Not Balanced";
+        return stack.empty() ? BALANCED : NOT_BALANCED;
     }
 }
