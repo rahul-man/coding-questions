ab16391dfec4c21b6c0e1a7e08e7c1f8ea89ffea
Rahul Mandal <mandalrahul52@gmail.com>
2024-09-10 19:39:46 +0530

    --more solutions

--- interview coding/src/main/java/com/java/codeinterview/strings/SumOfDigitAfterTransform.java
+++ interview coding/src/main/java/com/java/codeinterview/strings/SumOfDigitAfterTransform.java
@@ -3,40 +3,27 @@ package com.java.codeinterview.strings;
 public class SumOfDigitAfterTransform {
 
     public static void main(String[] args) {
-        String s = "zbax";
-        int k = 2;
-        int count = 0;
+        String s = "iiii";
         int sum = 0;
-        StringBuilder stringBuilder = new StringBuilder();
+        int k = 1;
         for (Character ch : s.toUpperCase().toCharArray()) {
             int digit = ch - 'A' + 1;
-            stringBuilder.append(digit);
+            sum += tranform(digit);
         }
-        System.out.println(stringBuilder);
-        String[] digits = stringBuilder.toString().split("");
-        sum = getSum(digits, sum);
-        count++;
-        while (count < k) {
-            sum = transFormAgain(sum);
-            count++;
+        while (k > 1) {
+            sum = tranform(sum);
+            k--;
         }
         System.out.println(sum);
     }
 
-    private static int transFormAgain(int sum) {
+    private static int tranform(int sum) {
         int total = 0;
-        while(sum > 0){
+        while (sum > 0) {
             int rem = sum % 10;
             total = total + rem;
             sum = sum / 10;
         }
         return total;
-    }
-
-    private static int getSum(String[] digits, int sum) {
-        for (String digit : digits) {
-            sum += Integer.parseInt(digit);
-        }
-        return sum;
     }
 }
